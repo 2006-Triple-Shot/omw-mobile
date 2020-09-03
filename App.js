67166cd0a6bfaa2aabac9e173f9f3dfac0465adf
esy <yenigun13@gmail.com>
2020-09-03 02:00:19 -0400

    rendered map with icon

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import MapView, {
+  PROVIDER_GOOGLE,
+  AnimatedRegion,
+  Marker,
+  Callout,
+} from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        let { status } = await Location.requestPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied");
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+      } catch (err) {
+        let status = Location.getProviderStatusAsync();
+        if (!(await status).locationServicesEnabled) {
+          alert("Enable location services");
+        }
+      }
+    })();
+  });
+
+  let text = "Waiting..";
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+  text = location;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start jwerking on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.mapStyle}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title={`San Francisco`}
+        >
+          <Callout>
+            <Text>{text}</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -17,5 +69,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
