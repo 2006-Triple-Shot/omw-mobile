047afd1548a9b0b8ab147a453328dd122a9a74c5
mcherry000 <cybermcherry000@gmail.com>
2020-09-04 17:09:02 -0400

    added suggestionA

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, TextInput } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-
+import apiKey from "./google-api";
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -9,6 +9,7 @@ export default class App extends Component {
       latitude: 0,
       longitude: 0,
       error: null,
+      destination: "",
     };
   }
 
@@ -25,6 +26,10 @@ export default class App extends Component {
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 2000 }
     );
   }
+  onChangeDestination(destination) {
+    this.setState({ destination });
+    const apiURL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}input=${destination}&location=37.785834, -122.406417&radius=2`;
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -36,7 +41,16 @@ export default class App extends Component {
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}
+          showsUserLocation={true}
         >
+          <TextInput
+            placeholder="Enter destination..."
+            style={styles.destinationInput}
+            value={this.state.destination}
+            onChangeText={(destination) =>
+              this.onChangeDestination(destination)
+            }
+          />
           <Marker coordinate={this.state} />
         </MapView>
       </View>
@@ -45,8 +59,21 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginRight: 5,
+    marginLeft: 5,
+    backgroundColor: "white",
+    padding: 5,
+  },
   container: {
     ...StyleSheet.absoluteFillObject,
+    // height: 400,
+    // width: 400,
+    // justifyContent: "flex-end",
+    // alignItems: "center",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
