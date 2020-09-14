fc880f45fb2a7ee07bda036d3cb909c4c3aadfaa
mcherry000 <cybermcherry000@gmail.com>
2020-09-13 22:11:26 -0400

    feat: working guest host index welcome

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
@@ -10,45 +9,38 @@ let hostSocket = null;
 let count = 0;
 let room = ["room 237"];
 const obj = { id: "", room: "" };
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   count++;
   console.log("-----Backend connected----", socket.id);
 
   socket.join(room, () => {
     const rooms = Object.keys(socket.rooms);
     const val = Object.values(socket.rooms);
-    console.log(rooms); // [ <socket.id>, 'room 237']
-    console.log(val); // [ <socket.id>, 'room 237']
   });
 
-  socket.on("joinEvent", guestLocation => {
+  socket.on("joinEvent", (guestLocation) => {
     guestSocket = socket;
     if (guestSocket !== null) {
       io.to("room 237").emit("start");
-
-      console.log("SOCKET -ID :", socket.id);
-      console.log("*****************");
     }
   });
 
-  socket.on("guestRequest", routeToHost => {
+  socket.on("guestRequest", (routeToHost) => {
     hostSocket = socket;
     console.log("Host wants a guest at ");
     if (guestSocket !== null) {
       io.to("room 237").emit("guestRequest", routeToHost);
-      // guestSocket.emit("guestRequest", routeToHost);
-      console.log("=======================");
     }
   });
 
-  socket.on("guestAccepts", guestLocation => {
+  socket.on("guestAccepts", (guestLocation) => {
     console.log("<<<<<<<<<<DRiver Accepted Backend>>>>>>>", guestLocation);
     if (hostSocket !== null) {
       hostSocket.emit("guestAccepts", guestLocation, socket.id);
     }
   });
 
-  socket.on("liveTracking", guestLocation => {
+  socket.on("liveTracking", (guestLocation) => {
     console.log("<<<<<<<<<<BACKGROUND DRIVER TRACKING >>>>>>>", guestLocation);
     if (hostSocket !== null) {
       hostSocket.emit("liveTracking", guestLocation, socket.id);
@@ -70,71 +62,3 @@ io.on("connection", socket => {
 server.listen(PORT, () => {
   console.log("server started and listening on port " + PORT);
 });
-
-
-
-
-// const express = require("express");
-// const app = express();
-// const server = require("http").createServer(app);
-// const io = require("socket.io").listen(server);
-// const PORT = process.env.PORT || 5000;
-
-// let guestSocket = null;
-// let hostSocket = null;
-// let count = 0;
-// let room = ["room 237"];
-// const obj = { id: "", room: "" };
-// io.on("connection", (socket) => {
-//   count++;
-//   console.log("-----Backend connected----", socket.id);
-
-//   socket.join(room, () => {
-//     const rooms = Object.keys(socket.rooms);
-//     const val = Object.values(socket.rooms);
-//   });
-
-//   socket.on("joinEvent", (guestLocation) => {
-//     guestSocket = socket;
-//     if (guestSocket !== null) {
-//       io.to("room 237").emit("start");
-//     }
-//   });
-
-//   socket.on("guestRequest", (routeToHost) => {
-//     hostSocket = socket;
-//     console.log("Host wants a guest at ");
-//     if (guestSocket !== null) {
-//       io.to("room 237").emit("guestRequest", routeToHost);
-//     }
-//   });
-
-//   socket.on("guestAccepts", (guestLocation) => {
-//     console.log("<<<<<<<<<<DRiver Accepted Backend>>>>>>>", guestLocation);
-//     if (hostSocket !== null) {
-//       hostSocket.emit("guestAccepts", guestLocation, socket.id);
-//     }
-//   });
-
-//   socket.on("liveTracking", (guestLocation) => {
-//     console.log("<<<<<<<<<<BACKGROUND DRIVER TRACKING >>>>>>>", guestLocation);
-//     if (hostSocket !== null) {
-//       hostSocket.emit("liveTracking", guestLocation, socket.id);
-//     }
-//   });
-
-//   socket.on("stopSharing", function (room) {
-//     try {
-//       console.log("[socket]", "leave room :", room);
-//       socket.leave(room);
-//       socket.to(room).emit("user left", socket.id);
-//     } catch (e) {
-//       console.log("[error]", "leave room :", e);
-//       socket.emit("error", "couldnt perform requested action");
-//     }
-//   });
-// });
-
-// server.listen(PORT, () => {
-//   console.log("server started and listening on port " + PORT);
-// });
--- src/components/screens/Welcome.js
+++ src/components/screens/Welcome.js
@@ -1,18 +1,16 @@
-
 import React, { Component } from "react";
 import { Button, StyleSheet, Text, View, Image } from "react-native";
 import NativeForms from "native-forms";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 class Welcome extends Component {
-
   render() {
     return (
       <View style={styles.container}>
-        <Image
+        {/* <Image
           source={require("/Users/esy/Fullstack-Academy/seniorPhase/capstone/On-My-Way/omw-mobile/src/components/images/lilbackboat.png")}
           style={styles.backgroundImage}
-        />
+        /> */}
 
         <TouchableOpacity
           onPress={() => this.props.navigation.navigate("Login")}
@@ -23,9 +21,8 @@ class Welcome extends Component {
         </TouchableOpacity>
       </View>
     );
-
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -56,45 +53,3 @@ const styles = StyleSheet.create({
 });
 
 export default Welcome;
-
-
-
-// import React, { useState } from "react";
-// import { Button, StyleSheet, Text, View } from "react-native";
-// import NativeForms from "native-forms";
-
-// const Welcome = () => {
-//   const [hasForm, showForm] = useState(false);
-//   const show = () => showForm(true);
-//   const hide = () => showForm(false);
-
-//   return (
-//     <View style={styles.container}>
-//       <Text>Host</Text>
-
-//       <Button
-//         title="Create Events | Invite Guests"
-//         onPress={show}
-//         color="#20f"
-//       />
-
-//       {hasForm && (
-//         <NativeForms
-//           form="https://my.nativeforms.com/wNy5UUa1jZmUmN3oUM31Db"
-//           onClose={hide}
-//         />
-//       )}
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
-
-// export default Welcome;
